69817ceb2ea6fed8cd589a083e4665c5ac2b209b
majonestube <martine.vold@hotmail.com>
2025-11-03 17:49:16 +0100

    Entities created

---
+++ TaskManagementAPI/Models/Entities/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagementAPI.Models.Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public int TaskItemId { get; set; }
+    public int UserId { get; set; }
+}
---
+++ TaskManagementAPI/Models/Entities/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManagementAPI.Models.Entities;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Status { get; set; }
+    public DateTime DueDate { get; set; }
+    public int ProjectId { get; set; }
+    public int AssignedUserId { get; set; }
+}
