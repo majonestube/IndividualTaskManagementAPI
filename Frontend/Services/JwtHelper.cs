9622991007c7686be94081f671319e31c86aa7be
majonestube <martine.vold@hotmail.com>
2025-11-29 12:19:29 +0100

    Added JwtHelper method to the pages and components. Cleaned up unneccesary imports.

--- Frontend/Services/JwtHelper.cs
+++ Frontend/Services/JwtHelper.cs
@@ -10,10 +10,15 @@ public class JwtHelper(IHttpClientFactory httpFactory, ProtectedSessionStorage s
     private readonly IHttpClientFactory _httpFactory = httpFactory;
     private readonly ProtectedSessionStorage _sessionStorage = sessionStorage;
 
-    public async Task<HttpClient> GetAuthenticatedClientAsync(string clientName = "TaskManagementAPI")
+    public async Task<string> GetAuthorizationTokenAsync()
     {
         var tokenResult = await _sessionStorage.GetAsync<string>("authToken");
         var token = tokenResult.Value;
+        return token;
+    }
+    public async Task<HttpClient> GetAuthenticatedClientAsync(string clientName = "TaskManagementAPI")
+    {
+        var token = await GetAuthorizationTokenAsync();
 
         var http = _httpFactory.CreateClient(clientName);
         
@@ -25,6 +30,7 @@ public class JwtHelper(IHttpClientFactory httpFactory, ProtectedSessionStorage s
 
         return http;
     }
+    
     public static string? GetUserId(string token)
     {
         var handler = new JwtSecurityTokenHandler();
