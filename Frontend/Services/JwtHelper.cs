6ee46fd77dbc97afacea682870c87be0597b517d
majonestube <martine.vold@hotmail.com>
2025-11-29 12:04:05 +0100

    Made helper method to set the authentication headers

--- Frontend/Services/JwtHelper.cs
+++ Frontend/Services/JwtHelper.cs
@@ -1,10 +1,30 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Net.Http.Headers;
 
 namespace Frontend.Services;
 
-public class JwtHelper
+public class JwtHelper(IHttpClientFactory httpFactory, ProtectedSessionStorage sessionStorage)
 {
+    private readonly IHttpClientFactory _httpFactory = httpFactory;
+    private readonly ProtectedSessionStorage _sessionStorage = sessionStorage;
+
+    public async Task<HttpClient> GetAuthenticatedClientAsync(string clientName = "TaskManagementAPI")
+    {
+        var tokenResult = await _sessionStorage.GetAsync<string>("authToken");
+        var token = tokenResult.Value;
+
+        var http = _httpFactory.CreateClient(clientName);
+        
+        if (!string.IsNullOrEmpty(token))
+        {
+            http.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        return http;
+    }
     public static string? GetUserId(string token)
     {
         var handler = new JwtSecurityTokenHandler();
