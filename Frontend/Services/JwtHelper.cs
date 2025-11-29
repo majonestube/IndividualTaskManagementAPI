8304c483d746fb0d5d291dc689c950fdc392e70d
majonestube <martine.vold@hotmail.com>
2025-11-29 11:33:06 +0100

    Delete button show for owner of a task

---
+++ Frontend/Services/JwtHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Frontend.Services;
+
+public class JwtHelper
+{
+    public static string? GetUserId(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var jwt = handler.ReadJwtToken(token);
+
+        return jwt.Claims.FirstOrDefault(c =>
+            c.Type == ClaimTypes.NameIdentifier ||
+            c.Type == "sub" ||
+            c.Type == "id"
+        )?.Value;
+    }
+}
