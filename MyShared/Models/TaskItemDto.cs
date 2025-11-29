f1fcfb492f0826f426baada5a68bcff8d2d712ff
majonestube <martine.vold@hotmail.com>
2025-11-29 19:34:44 +0100

    Changed layout of task card

--- MyShared/Models/TaskItemDto.cs
+++ MyShared/Models/TaskItemDto.cs
@@ -5,6 +5,7 @@ public class TaskItemDto
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public int StatusId { get; set; }
     public string Status { get; set; }
     public DateTime DueDate { get; set; }
     public int ProjectId { get; set; }
