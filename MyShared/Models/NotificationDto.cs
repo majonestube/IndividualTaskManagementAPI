7b7e54cdef740d7976fc9d98c99139cd6721a348
majonestube <martine.vold@hotmail.com>
2025-12-02 20:12:26 +0100

    Added linking to notification cards

--- MyShared/Models/NotificationDto.cs
+++ MyShared/Models/NotificationDto.cs
@@ -2,7 +2,9 @@
 
 public class NotificationDto
 {
+    public int ProjectId { get; set; }
     public string ProjectName { get; set; }
+    public int? TaskId { get; set; }
     public string? TaskName { get; set; }
     public string Message { get; set; }
     public DateTime Created { get; set; }
